29f2ee09dfb50667395fb73f7b95bde8e39c43b9
asunnyroy <150259266+asunnyroy@users.noreply.github.com>
2026-03-18 18:28:51 +0000

    converversion

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "true"
 
-console.log(typeof score) ;
-console.log(typeof (score));
+// console.log(typeof score) ;
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof score) ;
+// console.log(typeof score) ;
 
 console.log( valueInNumber);
 
@@ -27,3 +27,36 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+// ***************** Operations *************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+
+
+
+let str1 = "hello"
+let str2 = "sunny"
+str3 = str1 +str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "2")
+//  console.log(+true)
+// console.log(+"")
+
+let num1, num2 , num3
+num1= num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
