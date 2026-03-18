fcbdf144b9e4e969f17a87a2017256a39cbf381d
asunnyroy <150259266+asunnyroy@users.noreply.github.com>
2026-03-18 15:02:05 +0000

    dataTypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" ; // treat all js Code as newer version
+
+// alert (3+3) we are using nodeJs , not browser
+
+console.log(3 + 3) 
+
+console.log("sunny")
+
+let name = "sunny" //string
+
+let age = 22 // Number
+let isLoggedIn = true // boolean
+
+// number => 2 to power 53
+//bigint => for very larno
+//String => ""
+//boolean => true/ false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
