370fc111988060a07613bea8e90755b7e85c7bb5
asunnyroy <150259266+asunnyroy@users.noreply.github.com>
2026-03-29 19:08:08 +0000

    truthy value

--- Basic3/truthy.js
+++ Basic3/truthy.js
@@ -37,7 +37,7 @@ let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15 
-val1 = null ?? 10 ?? 15
+val1 = null ?? undefined ?? console.log("not proper value")
 
 console.log(val1);
 
