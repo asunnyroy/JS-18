2c5b80f46f714123c2446ed35f8ae0a2fea139dd
asunnyroy <150259266+asunnyroy@users.noreply.github.com>
2026-03-29 20:00:14 +0000

    read only

---
+++ 05_iterations/objloop.js
@@ -0,0 +1,28 @@
+const myObject ={
+    js:'Javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by aaple'
+}
+
+for (const key in myObject) {
+//   console.log(key);
+    // console.log(`${key}  :->shortcut is for ${myObject[key]}`)
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cppp"]
+
+for (const key in programming){
+    // console.log(programming[key])
+}
+
+//  const map = new Map()
+//  map.set('IN', "India")
+//  map.set('USA',"United States of America")
+//  map.set('Fr', "France")
+
+// for (const key in map) {
+//     console.log(key)
+// }
+
